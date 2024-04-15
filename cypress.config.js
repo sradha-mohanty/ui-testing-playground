a748529d5234b7969909c3e97173dbca8595fca0
sradha-mohanty <sradhamohanty93@gmail.com>
2024-04-15 10:00:20 +0200

    folder name updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  //reporter: 'cypress-mochawesome-reporter',
-  e2e: {
+  
+    reporter: 'cypress-mochawesome-reporter',
+   e2e: {
     baseUrl: 'https://uitestingplayground.com/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      //require('cypress-mochawesome-reporter/plugin')(on)
-    },
-  },
-});
+    require('cypress-mochawesome-reporter/plugin')(on);
+    }
+  }
+  });
--- cypress/components/sampleApp.cy.js
+++ cypress/components/sampleApp.cy.js
@@ -12,7 +12,6 @@ cy.get("#loginstatus").should("contain",data.username)
 it("Fill and submit the form without Name feild and check the message",()=>{
     cy.fixture("sampleApp_TestData.json").then((data)=>{
     cy.visit("sampleapp");
-    //cy.get(".form-control[name=UserName]").type(data.username);
     cy.get(".form-control[name=Password]").type(data.password);
     cy.get("#login").click();
     cy.get("#loginstatus").should("have.text",data.errormsg)
