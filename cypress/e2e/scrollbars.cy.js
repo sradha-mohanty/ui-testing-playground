f2bbc83a021a1c13b374728418ea7c31b464bf3e
sradha-mohanty <sradhamohanty93@gmail.com>
2024-04-15 13:13:46 +0200

    readme.md added

--- cypress/e2e/scrollbars.cy.js
+++ cypress/e2e/scrollbars.cy.js
@@ -1,4 +1,4 @@
-describe("Scrollbars", () => {
+describe("ScrollBars", () => {
     it("Scroll to get the button on page", () => {
       cy.visit("scrollbars");
       cy.get("#hidingButton").scrollIntoView().should("be.visible").click();
